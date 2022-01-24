751aecf0c52d9c0fb0b922722795f7db0706aa58
bahaaabed <bahaa.9999.sh@gmail.com>
2022-01-24 08:24:08 +0000

    create the salary components after install app

--- mosyr/install.py
+++ mosyr/install.py
@@ -1,22 +1,60 @@
 import frappe
 
 def after_install():
-    
+    create_salary_components()
+    create_salary_structure()
+    frappe.db.commit()
+
+def create_salary_components():
+    print('[*] Add Salary Components')
     components = [
-        {'component': 'Housing', 'abbr': 'H'},
-        {},
-        {}]
-    
-    {'Allowance Housing':'AH', 'Allowance Worknatural':'AW', 'Allowance Other':'AO',
-                 'Allowance Phone':'AP', 'Allowance Trans':'AT', 'Allowance Living':'AL'}
+        {'component': 'Housing',               'abbr': 'H'},
+        {'component': 'Allowance Housing',     'abbr':'AH'},
+        {'component': 'Allowance Worknatural', 'abbr':'AW'},
+        {'component': 'Allowance Other',       'abbr':'AO'},
+        {'component': 'Allowance Phone',       'abbr':'AP'},
+        {'component': 'Allowance Trans',       'abbr':'AT'},
+        {'component': 'Allowance Living',      'abbr':'AL'}]
                  
-    for comp ,desc in components.items():
-        salary_component = frappe.new_doc("Salary Component")
-        salary_component.salary_component = comp
-        salary_component.salary_component_abbr = desc
-        salary_component.type = "Earning"
-        salary_component.insert()
-        frappe.db.commit()
+    for component in components:
+        salary_component = component['component']
+        if not frappe.db.exists('Salary Component', salary_component):
+            salary_component_abbr = component['abbr']
+            component_type = "Earning"
+            salary_component_doc = frappe.new_doc("Salary Component")
+            salary_component_doc.update({
+                'salary_component': salary_component,
+                'salary_component_abbr': salary_component_abbr,
+                'type': component_type})
+            salary_component_doc.save()
 
-    
+def create_salary_structure():
+    print('[*] Add Salary Structure')
+    components = [
+        {'component': 'Housing',               'abbr': 'H'},
+        {'component': 'Allowance Housing',     'abbr':'AH'},
+        {'component': 'Allowance Worknatural', 'abbr':'AW'},
+        {'component': 'Allowance Other',       'abbr':'AO'},
+        {'component': 'Allowance Phone',       'abbr':'AP'},
+        {'component': 'Allowance Trans',       'abbr':'AT'},
+        {'component': 'Allowance Living',      'abbr':'AL'}]
+    salary_structure_doc = frappe.new_doc('Salary Structure')
+    company = frappe.defaults.get_global_default('company')
+    company = frappe.get_doc('Company', company)
+    salary_structure_doc.update({
+        '__newname': 'Base Salary Structure',
+        'is_active': 'Yes',
+        'payroll_frequency': 'Monthly',
+        'company': company.name,
+        'currency': company.default_currency
+    })
 
+    for component in components:
+        salary_component_abbr = component['abbr']
+        salary_component = component['component']
+        salary_structure_doc.append('earnings', {
+            'salary_component': salary_component,
+            'abbr': salary_component_abbr
+        })
+    salary_structure_doc.save()
+    salary_structure_doc.submit()
