e2d0bd5113a25e7e9aff5d42ebaefc8dab15a860
zaqoutabed <zaqoutabed@gmail.com>
2022-02-22 18:46:49 +0200

    edit contract doctype to make it easy to use

--- mosyr/mosyr/doctype/employee_contract/employee_contract.js
+++ mosyr/mosyr/doctype/employee_contract/employee_contract.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee Contract', {
-	// refresh: function(frm) {
-
-	// }
+    refresh: function(frm) {
+        if (frm.doc.docstatus == 1 && frm.doc.status == 'Not Applied') {
+            frm.add_custom_button('Apply In System', () => {
+                frappe.call({
+                    method: 'apply_in_system',
+                    doc: frm.doc,
+                    callback: () => {
+                        frm.reload_doc()
+                    }
+                })
+            })
+        }
+    }
 });
