1a1c0eb0a60282e73a8eabeb055e655c805a33c9
tahir-zaqout <zaqout2000@gmail.com>
2022-05-19 14:09:24 +0300

    create cash custody doctype

---
+++ mosyr/mosyr/doctype/cash_custody/cash_custody.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2022, AnvilERP and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class CashCustody(Document):
+	def on_submit(self):
+		self.estimated_value = self.custody_value
+		self.save()
+
