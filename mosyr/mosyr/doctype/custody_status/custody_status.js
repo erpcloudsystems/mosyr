26def3205c6803b4da91e6ada525b12471962eb3
zaqoutabed <zaqoutabed@gmail.com>
2022-06-01 17:12:06 +0300

    create custody status doctype

---
+++ mosyr/mosyr/doctype/custody_status/custody_status.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, AnvilERP and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Custody Status', {
+	// refresh: function(frm) {
+
+	// }
+});
