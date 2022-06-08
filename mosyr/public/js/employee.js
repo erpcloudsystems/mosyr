57db3b32babf6e7588b2e15838d13d5090fc3438
tahir-zaqout <zaqout2000@gmail.com>
2022-06-08 14:01:13 +0300

    set approvers for employee from department

--- mosyr/public/js/employee.js
+++ mosyr/public/js/employee.js
@@ -30,5 +30,23 @@ frappe.ui.form.on('Employee', {
         // frm.add_custom_button(__('Leave'), function() {
         //     frm.scroll_to_field('mosyr_employee_status');
         // }, __('Jump to'));
+    },
+    department: function(frm){
+        if (frm.doc.department){
+            frappe.call({
+                method: "mosyr.api.set_employee_approvers",
+                args:{
+                    department: frm.doc.department
+                },
+                callback: r =>{
+                    if (r.message){
+                        frm.doc.leave_approver = r.message[0]
+                        frm.doc.expense_approver = r.message[1]
+                        frm.doc.shift_request_approver = r.message[2]
+                        frm.refresh_fields()
+                    }
+                }
+            })
+        }
     }
 });
