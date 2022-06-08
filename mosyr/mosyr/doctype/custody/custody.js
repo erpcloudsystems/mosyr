efb038308d5e5b6df991ce5722b198ed48ddb684
tahir-zaqout <zaqout2000@gmail.com>
2022-06-08 17:05:57 +0300

    add custom button to return custody

--- mosyr/mosyr/doctype/custody/custody.js
+++ mosyr/mosyr/doctype/custody/custody.js
@@ -2,7 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Custody', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if (frm.doc.docstatus==1 && frm.doc.status != "Returned"){
+			frm.add_custom_button(__("Return Custody"), ()=>{
+				frappe.run_serially([
+					()=> frappe.new_doc("Return Custody"),
+					() => frappe.timeout(1),
+					()=> cur_frm.set_value("custody", frm.doc.name)
+				])
+			}).addClass('btn btn-danger')
+		}
+	}
 });
