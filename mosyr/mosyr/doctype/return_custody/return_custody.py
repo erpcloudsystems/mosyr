a24afb723a5fde7ea29a5f717176d03c177d62bc
tahir-zaqout <zaqout2000@gmail.com>
2022-06-09 16:19:48 +0300

    auto convert date from hijri to georgian

--- mosyr/mosyr/doctype/return_custody/return_custody.py
+++ mosyr/mosyr/doctype/return_custody/return_custody.py
@@ -5,6 +5,8 @@ import frappe
 from frappe import _
 from frappe.model.document import Document
 from frappe import sendmail, msgprint
+from frappe.utils import getdate
+from hijri_converter import Hijri, Gregorian
 
 class ReturnCustody(Document):
 	def on_submit(self):
@@ -26,3 +28,15 @@ class ReturnCustody(Document):
 				reference_name=self.custody,
 			)
 			msgprint(_("email sent successfully for {}".format(self.recipt_employee)))
+
+	@frappe.whitelist()
+	def convert_date(self, gregorian_date=None, hijri_date=None):
+		if gregorian_date:
+			gd, gm, gy = getdate(gregorian_date).day, getdate(gregorian_date).month, getdate(gregorian_date).year
+			hijri = Gregorian(gy, gm, gd).to_hijri()
+			return str(hijri)
+
+		if hijri_date:
+			hd, hm, hy = getdate(hijri_date).day, getdate(hijri_date).month, getdate(hijri_date).year
+			gregorian = Hijri(hy, hd, hm ).to_gregorian()
+			return str(gregorian)
