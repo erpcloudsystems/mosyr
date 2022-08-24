6d8cf7a4477cb10d9caa3ac77cbae90596690560
zaqoutabed <zaqoutabed@gmail.com>
2022-08-24 17:28:57 +0300

    fix contract workflow and prepare salaris structures

--- mosyr/mosyr/doctype/employee_contract/employee_contract.py
+++ mosyr/mosyr/doctype/employee_contract/employee_contract.py
@@ -1,220 +1,259 @@
 # Copyright (c) 2022, AnvilERP and contributors
 # For license information, please see license.txt
 
+from ast import arg
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.utils import cint, flt, nowdate
-import erpnext
+from frappe.utils import nowdate, flt, cint
+from mosyr.install import create_salary_components
+from erpnext.payroll.doctype.payroll_entry.payroll_entry import get_start_end_dates
 
 class EmployeeContract(Document):
 	def validate(self):
-		if self.contract_start_date > self.contract_end_date:
+		if self.contract_start_date >= self.contract_end_date:
 			frappe.throw(_("The end date of the contract must be after the start date"))
 			return
 		
-		previous_contracts = frappe.get_list('Employee Contract', filters={'employee': self.employee, 'docstatus' :1, 'contract_status': 'Valid', 'contract_start_date': ["<=", self.contract_end_date]})
+		previous_contracts = frappe.get_list('Employee Contract', 
+											 filters={
+												'employee': self.employee, 
+												'docstatus' :1, 
+												'status': 'Approved', 
+												'contract_status': 'Valid', 
+												'contract_start_date': ["<=", self.contract_end_date]})
 		if len(previous_contracts) > 0:
 			frappe.throw(_("Employee {} has Valid contract within {} and {}".format(self.employee_name, self.contract_start_date, self.contract_end_date)))
 			return
 		
 		if self.hiring_start_date < self.contract_start_date or self.hiring_start_date > self.contract_end_date:
-			if self.key:
-				frappe.msgprint(_("Hiring Date in contract {} is out of range {} and {}".format(self.key, self.contract_start_date, self.contract_end_date)))
-			elif self.name:
-				frappe.msgprint(_("Hiring Date in contract {} is out of range {} and {}".format(self.name, self.contract_start_date, self.contract_end_date)))
-			else:
-				frappe.msgprint(_("Hiring Date is out of range {} and {}".format(self.contract_start_date, self.contract_end_date)))
+			frappe.msgprint(_("Hiring Date in contract {} is out of range {} and {}".format(self.name, self.contract_start_date, self.contract_end_date)))
 
-		self.check_workflow_status()
+	# def on_submit(self):
+		# Make sure that all components are in the system
+		# create_salary_components()
+		# self.create_employee_salary_structure()
 	
-	def submit(self):
-		if self.contract_status == "Not Valid":
-			frappe.throw("Can't take any action, The Contract is Not Valid")
+	@frappe.whitelist()
+	def create_employee_salary_structure(self):
+		if(self.docstatus != 1): return
+		self.validate_dates()
+		# Make sure that all components are in the system
+		create_salary_components()
+		employee = frappe.get_doc("Employee", self.employee)
+		company = frappe.get_doc('Company', employee.company)
+		# bsc = frappe.get_doc('Salary Component', 'Basic')
+		
+		salary_components = [
+			{
+				"salary_component": "Basic",
+				"amount_based_on_formula": 1,
+				"formula": "base * 1"
+			},
+			{
+				"salary_component": "Variable",
+				"amount_based_on_formula": 1,
+				"formula": "variable * 1"
+			}
+		]
 
-	@frappe.whitelist()	
-	def apply_in_system(self, apply):
-		employee = frappe.get_doc('Employee', self.employee)
-		lookup_value = {
-			"active" : "Active",
-			"inactive" : "Inactive",
-			"leave" : "Left",
-			"sponsorshiptrans" : "Suspended",
-			"finalexit" : "Left"
-		}
-		if self.hiring_start_date:
-			# Check  Employee Data
-			
-			employee.date_of_joining = self.hiring_start_date
-			employee.status = 'Active'
-			employee.save()
-			frappe.db.commit()
-		
-		if apply:
-			ss = self.create_salary_structure(employee.first_name)
-			self.create_salary_structure_assignment(ss)
-			self.db_set('status', 'Applied In System', update_modified=False)
-			employee.status = lookup_value.get(employee.api_employee_status, 'Inactive')
-			employee.valid_data = 1
-			employee.from_api = 0
-			employee.save()
-			frappe.db.commit()
-			return 'Applied In System'
-	
-	def create_salary_structure_assignment(self, ss):
-		ssa = frappe.new_doc('Salary Structure Assignment')
-		ssa.employee = self.employee
-		ssa.company = ss.company
-		ssa.salary_structure = ss.name
-		ssa.from_date = self.contract_start_date
-		ssa.base = flt(self.allowance_trans_value)
-		ssa.save()
-		ssa.submit()
-		frappe.db.commit()
-		
-	
-	def create_salary_structure(self, employee):
-		
-		earnings = [{
-			'salary_component' : 'Basic',
-			'abbr': 'B',
-			'amount_based_on_formula': 1,
-			'formula': f'base'
-		}]
-		
-		if self.over_time_included == 'Yes':
-			earnings.append({
-				'salary_component' : 'Overtime',
-				'abbr': 'OT'
-			})
-		
-		if self.allowance_trans != 'None' and self.allowance_period == 'Monthly':
-			comp = {
-				'salary_component' : 'Allowance Trans',
-				'abbr': 'AT',
-			}
-			if self.allowance_trans_amount_type == 'Fixed':
-				comp.update({
-					'amount': flt(self.allowance_trans_value)
-				})
-			else:
-				if flt(self.allowance_trans_value) != 0:
-					comp.update({
-						'amount_based_on_formula': 1,
-						'formula': f'B * {flt(self.allowance_trans_value)}'
-					})
-			earnings.append(comp)
-		
-		if self.allowance_housing != 'None' and self.allowance_housing_schedule == 'Monthly':
-			comp = {
-				'salary_component' : 'Allowance Housing',
-				'abbr': 'AH',
-			}
-			if self.house_amount_type == 'Fixed':
-				comp.update({
-					'amount': flt(self.allowance_housing_value)
-				})
-			else:
-				if flt(self.allowance_housing_value) != 0:
-					comp.update({
-						'amount_based_on_formula': 1,
-						'formula': f'B * {flt(self.allowance_housing_value)}'
-					})
-			earnings.append(comp)
-		
-		if self.allowance_phone != 'None' and self.allowance_phone_schedule == 'Monthly':
-			comp = {
-				'salary_component' : 'Allowance Phone',
-				'abbr': 'AP',
-			}
-			if self.allowance_phone_amount_type == 'Fixed':
-				comp.update({
-					'amount': flt(self.allowance_phone_value)
-				})
-			else:
-				if flt(self.allowance_phone_value) != 0:
-					comp.update({
-						'amount_based_on_formula': 1,
-						'formula': f'B * {flt(self.allowance_phone_value)}'
-					})
-			earnings.append(comp)
-		
-		if self.allowance_worknatural != 'None' and self.allowance_worknatural_schedule == 'Monthly':
-			comp = {
-				'salary_component' : 'Allowance Worknatural',
-				'abbr': 'AW',
-			}
-			if self.allowance_worknatural_amount_type == 'Fixed':
-				comp.update({
-					'amount': flt(self.allowance_worknatural_value)
-				})
-			else:
-				if flt(self.allowance_worknatural_value) != 0:
-					comp.update({
-						'amount_based_on_formula': 1,
-						'formula': f'B * {flt(self.allowance_worknatural_value)}'
-					})
-			earnings.append(comp)
-			
-		if self.allowance_other != 'None' and self.allowance_other_schedule == 'Monthly':
-			comp = {
-				'salary_component' : 'Allowance Other',
-				'abbr': 'AO',
-			}
-			if self.allowance_other_amount_type == 'Fixed':
-				comp.update({
-					'amount': flt(self.allowance_other_value)
-				})
-			else:
-				if flt(self.allowance_other_value) != 0:
-					comp.update({
-						'amount_based_on_formula': 1,
-						'formula': f'B * {flt(self.allowance_other_value)}'
-					})
-			earnings.append(comp)
-		
-		if self.allowance_living != 'None' and self.allowance_living_schedule == 'Monthly':
-			comp = {
-				'salary_component' : 'Allowance Living',
-				'abbr': 'AL',
-			}
-			if self.field_allowance_feed_amount_typ == 'Fixed':
-				comp.update({
-					'amount': flt(self.allowance_living_value)
-				})
-			else:
-				if flt(self.allowance_living_value) != 0:
-					comp.update({
-						'amount_based_on_formula': 1,
-						'formula': f'base * {flt(self.allowance_living_value)}'
-					})
-			earnings.append(comp)
-		
-		details = {
-			"doctype": "Salary Structure",
-			"name":  f'{employee}',
-			"company": erpnext.get_default_company(),
-			# "earnings": make_earning_salary_component(setup=True,  test_tax=test_tax, company_list=["_Test Company"]),
-			# "deductions": make_deduction_salary_component(setup=True, test_tax=test_tax, company_list=["_Test Company"]),
-			"payroll_frequency": 'Monthly',
-			"currency": erpnext.get_default_currency()
-		}
-		
-		salary_structure_doc = frappe.get_doc(details)
-		for er in earnings:
-			salary_structure_doc.append('earnings', er)
-		
-		salary_structure_doc.insert()
-		salary_structure_doc.submit()
-		# frappe.db.commit()
-		return salary_structure_doc
+		is_sauid = False
 
+		# Saudi Employee
+		e_insurance_formula = "base * pension_on_employee / 100"
+		c_insurance_formula = "base * pension_on_company / 100"
 
-	def check_workflow_status(self):
-		if self.workflow_state == "Approved And Applied":
-			apply = True
-			self.apply_in_system(apply)
+		# Non Saudi Employee
+		e_risk_formula = "base * risk_on_employee / 100"
+		c_risk_formula = "base * risk_on_company / 100"
 
-		if self.workflow_state == "Approved And Not Applied":
-			apply = False
-			self.apply_in_system(apply)
+		# Allowance Housing component
+		if self.allowance_housing == "Cash":
+			if cint(employee.include_housing) == 0:
+				args = {
+					"salary_component": "Allowance Housing"
+				}
+				if self.allowance_housing_schedule == "Monthly":
+					if self.house_amount_type == "Percentage":
+						args.update({
+							'amount_based_on_formula': 1,
+							'formula': "base * {}".format(flt(self.allowance_housing_value/100, 2))
+						})
+
+						if employee.social_insurance_type == "Saudi":
+							e_insurance_formula = "base + ( base * {} ) )  * pension_on_employee / 100".format(flt(self.allowance_housing_value/100, 2))
+							c_insurance_formula = "base + ( base * {} ) )  * pension_on_company / 100".format(flt(self.allowance_housing_value/100, 2))
+						else:
+							e_risk_formula = "base + ( base * {} ) )  * risk_on_employee / 100".format(flt(self.allowance_housing_value/100, 2))
+							c_risk_formula = "base + ( base * {} ) )  * risk_on_company / 100".format(flt(self.allowance_housing_value/100, 2))
+					else:
+						args.update({
+							'amount_based_on_formula': 0,
+							"amount": self.allowance_housing_value
+						})
+
+						if employee.social_insurance_type == "Saudi":
+							e_insurance_formula = "( base + {} )  * pension_on_employee / 100".format(flt(self.allowance_housing_value, 2))
+							c_insurance_formula = "( base + {} )  * pension_on_company / 100".format(flt(self.allowance_housing_value, 2))
+						else:
+							e_risk_formula = "( base + {} )  * risk_on_employee / 100".format(flt(self.allowance_housing_value, 2))
+							c_risk_formula = "( base + {} ) * risk_on_company / 100".format(flt(self.allowance_housing_value, 2))
+								
+				salary_components.append(args)
+
+		# Allowance Transportations component
+		if self.allowance_trans == "Cash":
+			args = {
+				"salary_component": "Allowance Trans"
+			}
+			if self.allowance_period == "Monthly":
+				if self.allowance_trans_amount_type == "Percentage" and flt(self.allowance_trans_value) != 0:
+					args.update({
+						'amount_based_on_formula': 1,
+						'formula': "base * {}".format(flt(self.allowance_trans_value/100, 2))
+					})
+				else:
+					args.update({
+						'amount_based_on_formula': 0,
+						"amount": self.allowance_trans_value
+					})
+			salary_components.append(args)
+		
+		# Allowance Phone component
+		if self.allowance_phone == "Cash":
+			args = {
+				"salary_component": "Allowance Phone"
+			}
+			if self.allowance_phone_schedule == "Monthly":
+				if self.allowance_phone_amount_type == "Percentage" and flt(self.allowance_phone_value) != 0:
+					args.update({
+						'amount_based_on_formula': 1,
+						'formula': "base * {}".format(flt(self.allowance_phone_value/100, 2))
+					})
+				else:
+					args.update({
+						'amount_based_on_formula': 0,
+						"amount": self.allowance_phone_value
+					})
+			salary_components.append(args)
+		
+		# Allowance Nature of Work component
+		if self.allowance_worknatural == "Cash":
+			args = {
+				"salary_component": "Allowance Worknatural"
+			}
+			if self.allowance_worknatural_schedule == "Monthly":
+				if self.allowance_worknatural_amount_type == "Percentage" and flt(self.allowance_worknatural_value) != 0:
+					args.update({
+						'amount_based_on_formula': 1,
+						'formula': "base * {}".format(flt(self.allowance_worknatural_value/100, 2))
+					})
+				else:
+					args.update({
+						'amount_based_on_formula': 0,
+						"amount": self.allowance_worknatural_value
+					})
+			salary_components.append(args)
+		
+		# Allowance Living component
+		if self.allowance_living == "Cash":
+			args = {
+				"salary_component": "Allowance Living"
+			}
+			if self.allowance_living_schedule == "Monthly":
+				if self.field_allowance_feed_amount_typ == "Percentage" and flt(self.allowance_living_value) != 0:
+					args.update({
+						'amount_based_on_formula': 1,
+						'formula': "base * {}".format(flt(self.allowance_living_value/100, 2))
+					})
+				else:
+					args.update({
+						'amount_based_on_formula': 0,
+						"amount": self.allowance_living_value
+					})
+			salary_components.append(args)
+		
+		# Allowance Other component
+		if self.allowance_other == "Cash":
+			args = {
+				"salary_component": "Allowance Other"
+			}
+			if self.allowance_other_schedule == "Monthly":
+				if self.allowance_other_amount_type == "Percentage" and flt(self.allowance_other_value) != 0:
+					args.update({
+						'amount_based_on_formula': 1,
+						'formula': "base * {}".format(self.allowance_other_value)
+					})
+				else:
+					args.update({
+						'amount_based_on_formula': 0,
+						"amount": self.allowance_other_value
+					})
+			salary_components.append(args)
+		
+		ss_doc = frappe.new_doc("Salary Structure")
+		ss_doc.update({
+			"__newname": "{}-{}-{}".format(self.name, employee.name, nowdate()),
+			"is_active": "Yes",
+			"payroll_frequency": "Monthly",
+			"company": company.name,
+			"currency": company.default_currency
+		})
+
+		for salary_component in salary_components:
+			ss_doc.append("earnings", salary_component)
+
+		ss_doc.append("deductions", { "salary_component": "Other Deductions" })
+		if employee.social_insurance_type == "Saudi":
+			ss_doc.append("deductions", 
+							{
+								"salary_component": "Employee Pension Insurance",
+								"amount_based_on_formula": 1,
+								"formula": e_insurance_formula
+							})
+			ss_doc.append("deductions", 
+							{
+								"salary_component": "Company Pension Insurance",
+								"amount_based_on_formula": 1,
+								"formula": c_insurance_formula,
+								"do_not_include_in_total": 1,
+								"statistical_component": 1
+							})
+
+		else:
+			ss_doc.append("deductions", 
+							{
+								"salary_component": "Risk On Employee",
+								"amount_based_on_formula": 1,
+								"formula": e_risk_formula
+							})
+			ss_doc.append("deductions", 
+							{
+								"salary_component": "Risk On Company",
+								"amount_based_on_formula": 1,
+								"formula": c_risk_formula,
+								"do_not_include_in_total": 1,
+								"statistical_component": 1
+							})
+
+		ss_doc.save()
+		ss_doc.submit()
+
+		ssa_doc = frappe.new_doc("Salary Structure Assignment")
+		ssa_doc.employee = self.employee
+		ssa_doc.salary_structure = ss_doc.name
+		dates = get_start_end_dates("Monthly", self.contract_start_date)
+		ssa_doc.from_date = dates.start_date
+		ssa_doc.base = self.basic_salary
+		ssa_doc.save()
+		ssa_doc.submit()
+
+	def validate_dates(self):
+		class DuplicateAssignment(frappe.ValidationError): pass
+
+		if self.contract_start_date:
+			if frappe.db.exists(
+				"Salary Structure Assignment",
+				{"employee": self.employee, "from_date": self.contract_start_date, "docstatus": 1},
+			):
+				frappe.throw(_("Salary Structure Assignment for Employee already exists"), DuplicateAssignment)
