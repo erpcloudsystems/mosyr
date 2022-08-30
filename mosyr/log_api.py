1aab54b2d1d69a47aa7cb825870ade6686285989
zaqoutabed <zaqoutabed@gmail.com>
2022-08-30 16:56:37 +0300

    Open API to log biometric data

---
+++ mosyr/log_api.py
@@ -0,0 +1,15 @@
+import frappe
+
+@frappe.whitelist(allow_guest=True)
+def add_biometric_data(*args, **kwargs):
+    resuest_data = {}
+    resuest_data.update({
+        "kwargs": kwargs,
+        "args": args,
+        "req_headers": frappe.request.headers,
+        "req_data": frappe.request.data.decode(),
+        "req_values": frappe.request.values,
+        "req_args": frappe.request.args
+    })
+    frappe.logger("mosyr.biometric").debug(resuest_data)
+    return "Done :)"
