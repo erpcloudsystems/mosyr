cfe73de2be5ac9ae434ac771e8138c29da9ae2bb
zaqoutabed <zaqoutabed@gmail.com>
2022-09-06 15:52:21 +0300

    prepare salary structures using payroll register tool

---
+++ mosyr/mosyr/doctype/payroll_register_tool_employee/payroll_register_tool_employee.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, AnvilERP and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Payroll Register Tool Employee', {
+	// refresh: function(frm) {
+
+	// }
+});
