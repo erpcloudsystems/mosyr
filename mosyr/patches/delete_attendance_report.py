ac27cb882d7ac2c101504f0318ef2dd7d032ca1e
zaqoutabed <zaqoutabed@gmail.com>
2022-10-23 15:46:28 +0300

    patch to delete old report

---
+++ mosyr/patches/delete_attendance_report.py
@@ -0,0 +1,7 @@
+import frappe
+
+def execute():
+    if frappe.db.exists('Report', 'Attendance'):
+        field_doc = frappe.get_doc('Report', 'Attendance')
+        field_doc.delete()
+        frappe.db.commit()
