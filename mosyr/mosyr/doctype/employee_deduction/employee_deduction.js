d2e4d0ddecdea98743ca4751d4a5590bbe08ef80
kamalghanima <kamalghanima123@gmail.com>
2022-11-06 15:35:49 +0200

    remove create at field and set date mandatory and defualt value

--- mosyr/mosyr/doctype/employee_deduction/employee_deduction.js
+++ mosyr/mosyr/doctype/employee_deduction/employee_deduction.js
@@ -2,7 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Employee Deduction', {
-    // refresh: function(frm) {
-       
-    // }
+    setup: function(frm) {
+        if(!frm.doc.date){
+            frm.doc.date = frappe.datetime.get_today()
+        }
+    }
 });
