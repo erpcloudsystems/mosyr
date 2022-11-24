b0027c444cd823f315734c5309fb7dd129705ec7
zaqoutabed <zaqoutabed@gmail.com>
2022-11-24 17:30:12 +0300

    make error log

--- mosyr/cron.py
+++ mosyr/cron.py
@@ -1,9 +1,13 @@
 
 import frappe
-from frappe.model.naming import make_autoname
 
 def process_auto_attendance_for_all_shifts():
+
     shift_list = frappe.get_all("Shift Type", filters={"enable_auto_attendance": "1"}, pluck="name")
     for shift in shift_list:
-        doc = frappe.get_cached_doc("Shift Type", shift)
-        doc.process_auto_attendance()
+        doc = frappe.get_doc("Shift Type", shift)
+        try:doc.process_auto_attendance()
+        except Exception as e: frappe.log_error(
+					title="Error while processing auto attendance for Shift Type {0}".format(shift),
+					message=e, )
+    frappe.db.commit()
