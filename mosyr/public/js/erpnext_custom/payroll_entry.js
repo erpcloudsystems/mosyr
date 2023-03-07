dd452cd58a15e68909c6abe2349d95eb6ab3cab9
zaqoutabed <abdullahzaqout20@gmail.com>
2023-03-07 18:37:12 +0200

    fetch employee for multi select options

--- mosyr/public/js/erpnext_custom/payroll_entry.js
+++ mosyr/public/js/erpnext_custom/payroll_entry.js
@@ -17,5 +17,33 @@ make_bank_entry = function (frm) {
     }
 };
 
-frappe.ui.form.on("Payroll Entry", {});
+frappe.ui.form.on("Payroll Entry", {
+    refresh: function (frm) {
+        if (frm.doc.docstatus == 0) {
+            frm.clear_custom_buttons();
+            if (!frm.is_new()) {
+                frm.page.clear_primary_action();
+                frm.add_custom_button(__("Get Employees"),
+                    function () {
+                        frm.events.get_employee_details(frm);
+                    }
+                ).toggleClass('btn-primary', !(frm.doc.employees || []).length);
+            }
+            if ((frm.doc.employees || []).length && !frappe.model.has_workflow(frm.doctype)) {
+                frm.page.clear_primary_action();
+                frm.page.set_primary_action(__('Create Salary Slips'), () => {
+                    frm.save('Submit').then(() => {
+                        frm.page.clear_primary_action();
+                        frm.refresh();
+                        frm.events.refresh(frm);
+                    });
+                });
+            }
+        }
+        if (frm.doc.docstatus == 1) {
+            if (frm.custom_buttons) frm.clear_custom_buttons();
+            frm.events.add_context_buttons(frm);
+        }
+    },
+});
 
