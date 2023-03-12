172eecda0ee567cc91c74c010224ce1371ed9787
kamalgh <kamalraed@anvilerp.com>
2023-03-12 15:52:48 +0200

    create validation

--- mosyr/mosyr_self_service/doctype/exit_permission/exit_permission.js
+++ mosyr/mosyr_self_service/doctype/exit_permission/exit_permission.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Exit Permission', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.set_query("employee", function() {
+			return {
+                "filters": {
+                    "company": frm.doc.company
+                }
+            }
+		})
+	}
 });
