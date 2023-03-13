9ca068c488dbace0074bdeaf8e31c2690f89a22a
kamalgh <kamalraed@anvilerp.com>
2023-03-14 00:32:46 +0200

    create checkin from exit permission

--- mosyr/mosyr_self_service/doctype/exit_permission/exit_permission.py
+++ mosyr/mosyr_self_service/doctype/exit_permission/exit_permission.py
@@ -6,6 +6,8 @@ from frappe.model.document import Document
 from frappe import _
 from erpnext.hr.utils import validate_active_employee
 from frappe.utils import time_diff_in_hours
+from frappe.utils import get_datetime ,time_diff_in_seconds, get_time
+from erpnext.hr.doctype.shift_assignment.shift_assignment import get_actual_start_end_datetime_of_shift
 
 class ExitPermission(Document):
     def validate(self):
@@ -21,3 +23,25 @@ class ExitPermission(Document):
         if exit_hours <= 0:
             frappe.throw(_("To Time must be after From Time"))
         self.exit_hours = exit_hours
+
+    def on_submit(self):
+        def create_checkin(employee, log_type, time ):
+            doc = frappe.new_doc("Employee Checkin")
+            doc.employee = employee
+            doc.log_type = log_type
+            doc.time = time
+            doc.insert()
+            frappe.db.commit()
+        if self.workflow_state == "Approved by HR":
+            shift_actual_timings = get_actual_start_end_datetime_of_shift(
+                self.employee, get_datetime(self.from_time), True
+            )
+            if shift_actual_timings:
+                if shift_actual_timings[0] and shift_actual_timings[1]:
+                    shift_type = frappe.get_doc("Shift Type", self.shift)
+                    if time_diff_in_seconds(str(self.from_time), str(shift_type.start_time)) <= 0:
+                        if time_diff_in_seconds(str(self.to_time), str(shift_type.start_time)) >= 0:
+                            create_checkin(self.employee, "IN", f'{self.date} {shift_type.start_time}')
+                    if time_diff_in_seconds(str(self.from_time), str(shift_type.end_time)) <= 0:
+                        if time_diff_in_seconds(str(self.to_time), str(shift_type.end_time)) >= 0:
+                            create_checkin(self.employee, "OUT", f'{self.date} {shift_type.end_time}')
