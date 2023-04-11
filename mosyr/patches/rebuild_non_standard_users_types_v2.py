bdf60ff259d11604f3b5705594732d9e25f6c6c5
zaqoutabed <aazaqout@gmail.com>
2023-04-11 14:57:51 +0300

    delete all patch that update a permissions

--- mosyr/patches/rebuild_non_standard_users_types_v2.py
+++
@@ -1,4 +0,0 @@
-from mosyr.install import create_non_standard_user_types
-
-def execute():
-    create_non_standard_user_types()
--- mosyr/patches/setup_roles_for_reports_v2.py
+++
@@ -1,7 +0,0 @@
-from mosyr.install import allow_read_for_reports
-from erpnext.setup.install import create_custom_role
-
-def execute():
-    data = {"role": "SaaS Manager"}
-    create_custom_role(data)
-    allow_read_for_reports()
