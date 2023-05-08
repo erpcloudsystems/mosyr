5b0ba4a76e4581abb0d596c38093a56c8343f36e
tahirAnvil <tzaqout@anvilerp.com>
2023-05-08 16:06:09 +0300

    remove patches for txt file

--- mosyr/patches/employee_benefit_date_field.py
+++ mosyr/patches/employee_benefit_date_field.py
@@ -3,11 +3,11 @@ import datetime
 
 def execute():
     return
-    emp_benefites = frappe.get_all("Employee Benefit", filters={"docstatus": 1})
-    for row in emp_benefites:
-        emp_benefit = frappe.get_doc("Employee Benefit", row.get("name"))
+    # emp_benefites = frappe.get_all("Employee Benefit", filters={"docstatus": 1})
+    # for row in emp_benefites:
+    #     emp_benefit = frappe.get_doc("Employee Benefit", row.get("name"))
 
-        if emp_benefit.payroll_month and len(str(emp_benefit.payroll_month).split("-")) == 2:
-            date = datetime.datetime.strptime(f"{emp_benefit.payroll_month}-01", "%Y-%m-%d").date()
-            emp_benefit.db_set("payroll_month", date)
-            frappe.db.commit()
+    #     if emp_benefit.payroll_month and len(str(emp_benefit.payroll_month).split("-")) == 2:
+    #         date = datetime.datetime.strptime(f"{emp_benefit.payroll_month}-01", "%Y-%m-%d").date()
+    #         emp_benefit.db_set("payroll_month", date)
+    #         frappe.db.commit()
