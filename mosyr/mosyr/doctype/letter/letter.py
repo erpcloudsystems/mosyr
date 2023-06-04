91d0381b23bb9bfa6229e3c534103ca7ca5fe7e8
tahirAnvil <tzaqout@anvilerp.com>
2023-06-04 17:00:16 +0300

    add letter doctype with print formats

--- mosyr/mosyr/doctype/letter/letter.py
+++ mosyr/mosyr/doctype/letter/letter.py
@@ -1,8 +1,18 @@
-# Copyright (c) 2022, AnvilERP and contributors
+# Copyright (c) 2023, AnvilERP and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Letter(Document):
 	pass
+
+@frappe.whitelist()
+def set_DF_print_format(pr_name):
+	"""Set print format as default"""
+	if not frappe.get_list("Print Format", {"doc_type": "Letter", "name": pr_name}):
+		pr_name = ""
+	doctype = frappe.get_doc("DocType", "Letter")
+	doctype.db_set("default_print_format", pr_name)
+	frappe.reload_doctype("Letter")
+	frappe.db.commit()
