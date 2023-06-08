6ac0013ec84940afa862f01cc4de8524c2f09006
MaiIsmail <mai.mq.1995@gmail.com>
2023-06-08 13:33:24 +0300

    Fix Permission Issues

--- mosyr/mosyr/doctype/mosyr_form/mosyr_form.js
+++ mosyr/mosyr/doctype/mosyr_form/mosyr_form.js
@@ -8,16 +8,16 @@ frappe.ui.form.on('Mosyr Form', {
 				frm.add_child('permissions', { role: 'System Manager' });
 			}
 		}
+	},
+	refresh: function (frm) {
+		let roles = ["Saas Manager", "Self Service", "SaaS User"]
 		frm.set_query("allowed","workflow_transition", function(doc) {
 			return {
 				filters: {
-					"is_custom": 1,
-					"name": ["!=", "Read User Type"]
+					name: ["in", roles],
 				}
 			};
 		});
-	},
-	refresh: function (frm) {
 		if (!frm.is_new() && !frm.doc.istable && frm.doc.docstatus == 1) {
 			frm.add_custom_button(__('Go to {0} List', [frm.doc.form_title]), () => {
 				window.open(`/app/${frappe.router.slug(frm.doc.name)}`);
