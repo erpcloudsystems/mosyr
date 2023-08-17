c60ceea71c110fd364967cfb3988cc43b2445e4b
MaiIsmail <mai.mq.1995@gmail.com>
2023-08-17 04:18:51 +0300

    Create Employee Shift Change Tool - v2

--- mosyr/mosyr/doctype/employee_shift_change_tool/employee_shift_change_tool.js
+++ mosyr/mosyr/doctype/employee_shift_change_tool/employee_shift_change_tool.js
@@ -17,14 +17,6 @@ frappe.ui.form.on('Employee Shift Change Tool', {
 					</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td>Tessst</td>
-							<td>Tessst</td>
-							<td>Tessst</td>
-							<td>Tessst</td>
-							<td>Tessst</td>
-							<td><button class="btn btn-primary btn-sm update-shift" data-item-code="xxxx">Update</button></td>
-						</tr>
 					</tbody>
 				</table>
 			</div>
@@ -32,11 +24,6 @@ frappe.ui.form.on('Employee Shift Change Tool', {
 	},
 	refresh: function (frm) {
 		frm.trigger("render_employees_shift_table")
-		$(".update-shift").on("click", function (e) {
-			console.log("78888888888888888888888888888888888888");
-			var id = $(this).data('item-code');
-			console.log('ID is ' + id);
-		});
 	},
 	render_employees_shift_table: function (frm) {
 		frappe.call({
@@ -47,13 +34,12 @@ frappe.ui.form.on('Employee Shift Change Tool', {
 			},
 			callback: function (r) {
 				if (!r.exc) {
-					$(`#employees_shift_table tbody`).html("")
+					// $(`#employees_shift_table tbody`).html("")
 					if (r.message.data.length > 0) {
 						r.message.data.forEach(row => {
 							if (row.emp_shifts.length > 1) {
 								row.emp_shifts.forEach((d, idx) => {
-									if(idx == 0) {
-										console.log("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM", idx);
+									if (idx == 0) {
 										$(`#employees_shift_table tbody`).append(`
 											<tr>
 												<td rowspan="${row.emp_shift_count}" class="bold">${d.employee_name}</td>
@@ -61,18 +47,17 @@ frappe.ui.form.on('Employee Shift Change Tool', {
 												<td>${d.end_date}</td>
 												<td>${d.shift_type}</td>
 												<td>${d.status}</td>
-												<td><button class="btn btn-primary btn-sm update-shift" data-sha-id="${d.name}">Update</button></td>
+												<td><button class="btn btn-primary btn-sm update-shift" data-employee="${d.employee}" data-sha-id="${d.name}">Update</button></td>
 											</tr>
 										`)
-									}else {
-										console.log("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii", idx);
+									} else {
 										$(`#employees_shift_table tbody`).append(`
 											<tr>
 												<td>${d.start_date}</td>
 												<td>${d.end_date}</td>
 												<td>${d.shift_type}</td>
 												<td>${d.status}</td>
-												<td><button class="btn btn-primary btn-sm update-shift" data-sha-id="${d.name}">Update</button></td>
+												<td><button class="btn btn-primary btn-sm update-shift" data-employee="${d.employee}" data-sha-id="${d.name}">Update</button></td>
 											</tr>
 										`)
 									}
@@ -86,14 +71,75 @@ frappe.ui.form.on('Employee Shift Change Tool', {
 										<td>${shift.end_date}</td>
 										<td>${shift.shift_type}</td>
 										<td>${shift.status}</td>
-										<td><button class="btn btn-primary btn-sm update-shift" data-sha-id="${shift.name}">Update</button></td>
+										<td><button class="btn btn-primary btn-sm update-shift" data-employee="${shift.employee}" data-sha-id="${shift.name}">Update</button></td>
 									</tr>
 								`)
 							}
 						});
 					}
+					$(".update-shift").on("click", function (e) {
+						var shift_id = $(this).data('sha-id');
+						var employee = $(this).data('employee');
+						frm.events.open_change_shift_dialoge(frm, employee);
+					});
 				}
 			}
 		})
+	},
+	open_change_shift_dialoge: function (frm, employee) {
+		var d = new frappe.ui.Dialog({
+			title: __('Select New Shift'),
+			fields: [
+				{
+					"label": "Shift",
+					"fieldname": "shift_name",
+					"fieldtype": "Link",
+					"options": "Shift Type",
+					"reqd": 1
+				},
+				{
+					"label": "Start Date",
+					"fieldname": "start_date",
+					"fieldtype": "Date",
+					"reqd": 1
+				},
+				{
+					"label": "End Date",
+					"fieldname": "end_date",
+					"fieldtype": "Date",
+					"reqd": 1
+				},
+				{
+					"label": "Recalculate the previous period",
+					"fieldname": "recalculate_period",
+					"fieldtype": "Check",
+					"defualt": 0
+				}
+			],
+			primary_action: function () {
+				var data = d.get_values();
+				frappe.call({
+					method: "update_employee_shift_and_recalculate_period",
+					doc: frm.doc,
+					args: {
+						employee: employee,
+						new_shift: data.shift_name,
+						start_date: data.start_date,
+						end_date: data.end_date,
+						recalculate_period: data.recalculate_period
+					},
+					callback: function (r) {
+						if (!r.exc) {
+							if (r.message) {
+								cur_frm.reload_doc();
+							}
+							d.hide();
+						}
+					}
+				});
+			},
+			primary_action_label: __('Save')
+		});
+		d.show();
 	}
 });
--- mosyr/mosyr/doctype/employee_shift_change_tool/employee_shift_change_tool.py
+++ mosyr/mosyr/doctype/employee_shift_change_tool/employee_shift_change_tool.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
@@ -17,7 +18,7 @@ class EmployeeShiftChangeTool(Document):
         if emp_list:
             for emp in emp_list:
                 shifts = frappe.db.get_list("Shift Assignment", fields=[
-                                            "name", "employee_name", "start_date", "end_date", "status", "shift_type"], filters={"employee": emp.get("name")})
+                                            "name", "employee", "employee_name", "start_date", "end_date", "status", "shift_type"], filters={"employee": emp.get("name")})
                 if len(shifts):
                     result.append({
                         "emp": emp.get("name"),
@@ -27,3 +28,38 @@ class EmployeeShiftChangeTool(Document):
                     })
 
         return {"data": result}
+
+    @frappe.whitelist()
+    def update_employee_shift_and_recalculate_period(self, employee, new_shift, start_date, end_date, recalculate_period):
+        if frappe.db.exists("Shift Type", new_shift):
+            if frappe.db.exists("Shift Assignment", {"employee": employee, "shift_type": new_shift, "status": "Active"}):
+                shift = frappe.get_doc("Shift Assignment", {
+                                       "employee": employee, "shift_type": new_shift, "status": "Active"})
+            else:
+                try:
+                    new_doc = frappe.new_doc("Shift Assignment")
+                    new_doc.employee = employee
+                    new_doc.shift_type = new_shift
+                    new_doc.start_date = start_date
+                    new_doc.end_date = end_date
+                    new_doc.save()
+                    new_doc.submit()
+                    if recalculate_period:
+                        # Get All missed Employee Checkin
+                        sql = """
+                            SELECT name
+                            FROM `tabEmployee Checkin`
+                            WHERE employee = '{0}' AND shift IS NULL AND DATE(time) > '{1}' AND DATE(time) < '{2}'
+                        """.format(employee, start_date, end_date)
+                        checkin_list = frappe.db.sql(sql, as_dict=1)
+                        for row in checkin_list:
+                            doc = frappe.get_doc(
+                                "Employee Checkin", row.get("name"))
+                            doc.save()
+                        frappe.db.commit()
+
+                        shift_doc = frappe.get_cached_doc(
+                            "Shift Type", new_shift)
+                        shift_doc.process_auto_attendance()
+                except Exception as e:
+                    frappe.log_error(title=_("Error while processing Auto Attendence "), message=e)
